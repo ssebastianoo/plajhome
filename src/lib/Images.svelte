1dcf916a5d0aba82f0457d22d0b660870c169910
ssebastianoo <sebastianogirotto05@gmail.com>
2022-07-05 15:31:56 +0200

    image slider

--- src/lib/Images.svelte
+++ src/lib/Images.svelte
@@ -1,72 +1,65 @@
 <script>
+	let index = 0;
 	const images = [
-		'VFL06437-min.JPG',
-		'VFL06441-min.JPG',
-		'VFL06443-min.JPG',
-		'VFL06444-min.JPG',
-		'VFL06457-min.JPG',
-		'VFL06459-min.JPG',
-		'VFL06462-min.JPG',
-		'VFL06465-min.JPG',
-		'VFL06467-min.JPG',
-		'VFL06471-min.JPG',
-		'VFL06472-min.JPG',
-		'VFL06473-min.JPG',
-		'VFL06474-min.JPG',
-		'VFL06475-min.JPG',
-		'VFL06476-min.JPG'
+		'VFL06437-min.webp',
+		'VFL06441-min.webp',
+		'VFL06443-min.webp',
+		'VFL06444-min.webp',
+		'VFL06457-min.webp',
+		'VFL06459-min.webp',
+		'VFL06462-min.webp',
+		'VFL06465-min.webp',
+		'VFL06467-min.webp',
+		'VFL06471-min.webp',
+		'VFL06472-min.webp',
+		'VFL06473-min.webp',
+		'VFL06474-min.webp',
+		'VFL06475-min.webp',
+		'VFL06476-min.webp'
 	];
 </script>
 
 <div class="images">
-	{#each images as image}
-		<div class="image">
-			<img src={'./house/' + image} alt="img" />
-		</div>
-	{/each}
+	<img src={'./house/' + images[index]} alt="Apartment" />
+	<div class="controls">
+		<button class="prev" on:click={() => (index = (index - 1 + images.length) % images.length)}>
+			<i class="fas fa-chevron-left" />
+		</button>
+		<p>{index + 1} / {images.length}</p>
+
+		<button class="next" on:click={() => (index = (index + 1) % images.length)}>
+			<i class="fas fa-chevron-right" />
+		</button>
+	</div>
 </div>
 
 <style lang="scss">
 	.images {
-		margin-top: 200px;
-		width: 94vw;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		padding: 0 3vw;
+		justify-content: center;
+		height: 500px;
 	}
-	.image {
+
+	img {
+		width: 90vw;
+		max-width: 400px;
+	}
+
+	.controls {
+		margin-top: 40px;
 		display: flex;
-		align-items: center;
-		margin: 100px;
-		width: 100%;
-		max-width: 1000px;
+		gap: 50px;
 
-		img {
-			width: 400px;
-		}
-
-		&:nth-child(odd) {
-			justify-content: right;
-		}
-	}
-
-	@media (max-width: 750px) {
-		.images {
-			margin-top: 50px;
-
-			.image {
-				margin: 50px 0;
-				justify-content: center;
-
-				&:nth-child(odd) {
-					justify-content: center;
-				}
-
-				img {
-					width: 300px;
-				}
-			}
+		button {
+			background: none;
+			border: none;
+			padding: 0;
+			margin: 0;
+			font-size: 2em;
+			color: #fff;
+			cursor: pointer;
 		}
 	}
 </style>

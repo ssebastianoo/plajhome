28f5490549427d65dc3685d413c728c28973f39e
ssebastianoo <sebastianogirotto05@gmail.com>
2022-08-13 17:36:15 +0200

    animation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Main from '$lib/Main.svelte';
 	import Images from '$lib/Images.svelte';
+	import Intro from '$lib/Intro.svelte';
 </script>
 
+<Intro />
 <Main title="plajhome"
 	>Luxury Open Space 11th floor 180° Venice view. Enjoy your holiday in Venice, live your confort.</Main
 >

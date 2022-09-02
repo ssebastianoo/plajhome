e2f6215057f9a8def219f0d37e3370193c42f68d
ssebastianoo <sebastianogirotto05@gmail.com>
2022-09-02 17:05:02 +0200

    astro

--- src/routes/contact.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Main from '$lib/Main.svelte';
-</script>
-
-<Main title="contact" image="./venice/VFL06411.jpg">
-	email: <a href="mailto:jureplaj@gmail.com">jureplaj@gmail.com</a>
-	<br><br>
-	mob: +39 3662551394
-</Main>
